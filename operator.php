b8d63895af1e344569ef51027905260f5e147c85
FalyaCharisma <falyacharisma9@gmail.com>
2023-05-23 08:03:32 +0700

    operator

---
+++ operator.php
@@ -0,0 +1,35 @@
+<?php
+    // Operator Aritmatika
+    // +, -, *, /, %
+    $a = 5;
+    $b = 2;
+    echo $a + $b;
+    // echo $a - $b;
+    // echo $a * $b;
+    // echo $a / $b;
+    // echo $a % $b;
+
+    // Menggabungkan teks
+    // .
+    $nama_depan = "Falya";
+    $nama_belakang = "Ayza";
+    echo $nama_depan . " " . $nama_belakang;
+
+    // Operator Assignment
+    // =, +=, -=, *=, /=, %= .=
+    $x = 1;
+    $x += 5;
+    echo $x;
+
+    // Operator Perbandingan
+    // <, >, <=, >=, ==, !=
+    var_dump(2 == "2");
+
+    // Operator Identitas
+    // ===, !==
+    var_dump(2 === "2");
+
+    // Operator Logika
+    // &&, ||
+    var_dump( 2 > 1 && 1 < 2);
+?>
