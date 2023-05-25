f47386d378dc9cc4041d0e9397832901a3dbfd7d
FalyaCharisma <falyacharisma9@gmail.com>
2023-05-25 23:41:19 +0700

    post

---
+++ post.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="post2.php" method="post">
+            Masukkan nama:
+            <input type="text" name="nama">
+            <br>
+            <button type="submit" name="simpan">Kirim!</button>
+        </form>
+    </body>
+</html>
---
+++ post2.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>Selamat datang <?= $_POST["nama"] ?></h1>
+    </body>
+</html>
